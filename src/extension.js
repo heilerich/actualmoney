ec860b5239c35572e36bb9ee55983203b83c1abf
Felix Heilmeyer <code@fehe.eu>
2019-11-02 20:30:44 +0100

    fixed app packaging and paths in release version

--- src/extension.js
+++ src/extension.js
@@ -1,17 +1,19 @@
 const fs = require('fs')
 const os = require('os')
 const {promisify} = require('util')
+const path = require('path')
 
 const copyFile = promisify(fs.copyFile)
 const readdir = promisify(fs.readdir)
 
-const targetDir = os.homedir() + '/Library/Containers/com.moneymoney-app.retail/Data/Library/Application Support/MoneyMoney/Extensions/'
-const sourceDir = 'lua/'
+const targetDir = path.join(os.homedir(), '/Library/Containers/com.moneymoney-app.retail/Data/Library/Application Support/MoneyMoney/Extensions/')
+const sourceDir = path.resolve(__dirname, '../lua/')
+
 const scriptName = 'AcM Export.lua'
 const versionPattern = /Exporter\{version\s+=\s+([0-9\.]+),/
 
 const getVersion = function(folder) {
-    return fs.readFileSync(folder + scriptName, 'utf8').match(versionPattern)[1]
+    return fs.readFileSync(path.join(folder, scriptName), 'utf8').match(versionPattern)[1]
 }
 
 const checkExtensionVersion = function() {
@@ -33,7 +35,7 @@ const copyScripts = async function() {
     let scripts = (await readdir(sourceDir, { withFileTypes: true }))
         .filter(dirent => dirent.isFile()).map(dirent => dirent.name)
     await Promise.all(scripts.map( async (script) => {
-        await copyFile(sourceDir + script, targetDir + script)
+        await copyFile(path.join(sourceDir, script), path.join(targetDir + script))
     }))
 }
 exports.checkExtensionVersion = checkExtensionVersion
